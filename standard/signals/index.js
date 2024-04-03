d57512309a867e83c02924ed1bfa7baab75250b9
webreflection <andrea.giammarchi@gmail.com>
2024-04-03 06:37:06 +0200

    Added effect

--- standard/signals/index.js
+++ standard/signals/index.js
@@ -1,16 +1,19 @@
-import { computed, signal } from './signal.js';
+import { computed, effect, signal } from './signal.js';
 
 const counter = signal(0);
 
-const isEven = computed(() => {
-  return (counter & 1) == 0;
+const isEven = computed(() => (counter & 1) == 0);
+
+effect(() => {
+  if (isEven.value)
+    console.log('is even');
+  else
+    console.log('is NOT even');
 });
 
-console.log(isEven.peek());
-console.log(isEven.value);
-
+// is even
 counter.value++;
-console.log(isEven.value);
+// is NOT even
 
-counter.value++;
-console.log(isEven.value);
+effect(() => console.log('fx', isEven.value, counter.value++));
+console.log('out', isEven.value, counter.value++); // 1
--- standard/signals/signal.js
+++ standard/signals/signal.js
@@ -1,51 +1,65 @@
-// TODO: effect and batch to have an almost 1:1 @webreflection/signal
-
 import { Signal as $Signal } from "signal-polyfill";
+// ONE DAY // const { Signal: $Signal } = globalThis;
 
-const { untrack } = $Signal.subtle;
+const {
+  Computed: C,
+  State: S,
+  subtle: { Watcher: W, untrack }
+} = $Signal;
 
+// for `instanceof Signal` operations (aka: isSignal)
 export class Signal {
-  /**
-   * @param {$Signal.Computed | $Signal.State} signal
-   */
-  constructor(signal) {
-    this._ = signal;
-  }
-
-  // IMPLICIT SUBSCRIPTION
-  [Symbol.toPrimitive]() {
-    return this._.get();
-  }
-  valueOf() {
-    return this._.get();
-  }
-
-  // EXPLICIT NO SUBSCRIPTION
-  peek() {
-    return untrack(() => this._.get());
-  }
-  toJSON() {
-    return untrack(() => this._.get());
-  }
+  constructor(_) { this._ = _ }
+  // signal == value
+  [Symbol.toPrimitive]() { return this._.get() }
+  valueOf() { return this._.get() }
+  // the beauty of previous APIs (libraries or standard)
+  peek() { return untrack(() => this._.get()) }
+  toJSON() { return untrack(() => this._.get()) }
 }
 
 class Computed extends Signal {
-  get value() {
-    return this._.get();
-  }
+  get value() { return this._.get() }
 }
 
 class State extends Signal {
-  get value() {
-    return this._.get();
-  }
-  set value(value) {
-    this._.set(value);
-  }
+  get value() { return this._.get() }
+  set value(value) { this._.set(value) }
 }
 
-export const computed = (callback, ...rest) => new Computed(new $Signal.Computed(callback, ...rest));
+const computed = (callback, ...rest) => new Computed(new C(callback, ...rest));
 
-export const signal = (value, ...rest) => new State(new $Signal.State(value, ...rest));
+const signal = (value, ...rest) => new State(new S(value, ...rest));
 
-export { untrack };
+// async effect related (aka: auto batch)
+let processPending = true;
+
+const w = new W(() => {
+  if (processPending) {
+    processPending = false;
+    queueMicrotask(pending);
+  }
+});
+
+const pending = () => {
+  processPending = true;
+  for (const s of w.getPending()) s.get();
+  w.watch();
+};
+
+const effect = callback => {
+  let cleanup = null;
+  const computed = new C(() => {
+    typeof cleanup === 'function' && cleanup();
+    cleanup = callback();
+  });
+  w.watch(computed);
+  computed.get();
+  return () => {
+    w.unwatch(computed);
+    typeof cleanup === 'function' && cleanup();
+    cleanup = null;
+  };
+};
+
+export { computed, effect, signal, untrack };
